0bf10800bafd0071ce76c1fe045d19c41865d89c
Akira Sunsets <132906123+AkiraSunsets@users.noreply.github.com>
2025-09-12 11:26:07 -0300

    Create CompraUsuario.java

---
+++ Lista 2 - Condicionais/CompraUsuario.java
@@ -0,0 +1,20 @@
+/*
+    Pergunte ao usuário o valor unitário de um produto e a quantidade que ele quer
+    comprar desse produto. Se a quantidade for até 12, o valor do produto é valor 
+    unitário normal vezes a quantidade. Se ele comprar mais do que 12, o valor terá 
+    um desconto de 10% ou o valor total da compra. Mostre o valor da compra baseado nessas
+    condições.
+ */
+
+public class CompraUsuario {
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo!");
+
+        System.out.println("Qual produto você gostaria de comprar?");
+        String nomeProduto = nextLine();
+        System.out.println("Qual o valor dele?");
+        int valorProduto = nextInt();
+        System.out.println("Quantas unidades você gostaria de comprar deste produto?");
+        int quantidadeProduto = nextInt();
+    }
+}
